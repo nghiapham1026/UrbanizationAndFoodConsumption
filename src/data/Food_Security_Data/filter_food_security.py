b02031e0eb32a9abd496cfacc7d24c1cad51c8a5
Nathan Pham <nghiapham102603@gmail.com>
2024-01-25 23:35:54 -0800

    Filter food security data on 4 main fields

--- src/data/Food_Security_Data/filter_food_security.py
+++ src/data/Food_Security_Data/filter_food_security.py
@@ -11,9 +11,16 @@ else:
     # List of South Asian countries to include
     south_asian_countries = ['India', 'Pakistan', 'Bangladesh', 'Nepal', 'Bhutan']
 
-    # Filter the data to include only the selected South Asian countries
-    filtered_new_data = new_data[new_data['Area'].isin(south_asian_countries)]
+    # List of fields to filter
+    fields_to_include = ["Average dietary energy requirement (kcal/cap/day)", 
+                         "Number of people undernourished (million) (3-year average)",
+                         "Number of children under 5 years of age who are overweight (modeled estimates) (million)",
+                         "Number of obese adults (18 years and older) (million)"]
+
+    # Filter the data to include only the selected South Asian countries and specified fields
+    filtered_new_data = new_data[new_data['Area'].isin(south_asian_countries) & 
+                                 new_data['Item'].isin(fields_to_include)]
 
     # Save the filtered data to a new CSV file
-    output_new_file_path = r"..\..\..\data\processed\Food_Security_Data\Food_Security_Data_S_Asia_NOFLAG.csv"
+    output_new_file_path = r"..\..\..\data\processed\Food_Security_Data\Food_Security_S_Asia_NOFLAG.csv"
     filtered_new_data.to_csv(output_new_file_path, index=False)
